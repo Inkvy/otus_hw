8ca8db41155188756d5373714b8160e5ddca2123
a-shumak <a-shumak@team-yandex.ru>
2019-11-25 19:47:30 +0700

    Первая задача. Наброски

---
+++ hw1/log_analyzer.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
+#                     '$status $body_bytes_sent "$http_referer" '
+#                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
+#                     '$request_time';
+
+import os
+import gzip
+import re
+
+config = {
+    "REPORT_SIZE": 1000,
+    "REPORT_DIR": "./reports",
+    "LOG_DIR": "./log"
+}
+
+
+def main():
+    files = [] # there will bee files in folder ./log
+    pattern_url = []  # there is will be  urls. Later remade to dict, where will be different information from each string
+    #check existing the folder
+    os.path.exists('./log')
+    #get list files includes in folder
+    list_files = os.listdir('./log')
+    
+    #get only files with 'nginx'
+    for file in list_files:
+         if 'nginx-access-ui' in file:
+             files.append(file)
+
+    # sorting list to get the last row
+    files.sort()
+    newest_log = files[-1]
+
+    # create path at newest log
+    path = './log/'+newest_log
+
+    if '.gz' in newest_log:    
+    #output all lines in gz-file
+        with gzip.open(path, 'rb') as f:
+            for line in f:
+                print(line.decode().strip())
+    else:
+        with open(path, 'r+') as f:
+            for line in f:
+                get_url = re.search(r'GET(.*)HTTP', line).group(1)
+                # print(line.strip( ))
+                print(line)
+                pattern_url.append(get_url)
+    print(pattern_url)
+    print(len(pattern_url))
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+"""
+
+Логи лежать в папке "log"
+1. Парсить логи по двум критериям: а) сервис(nginx), б) дата (получаю после сортировки свежий файл, но всё равно нужна дата)
+2. После того как выбрали все даты нужного сервиса. Нужно найти самую свежую дату
+3. Нашли дату - нужно открыть этот файл
+4. Файл может быть как plain text, так и gz(архив)
+5. Парсить строки файла
+
+- разобрать строку через регулярное выражение
+
+- когда фильтрую по названи файла, то использовать метод "начинается с"
+
+- когда накапливаю данные, то использовать словари и setdefault
+
+Автоматизация рутинных задач
+стр. 223, 423
+"""
